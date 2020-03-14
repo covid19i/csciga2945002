cd914304b3365714f35d32baf5d08e77d93d934a
covid19i <62162060+covid19i@users.noreply.github.com>
2020-03-13 22:23:16 -0400

    Update MMult1.cpp

--- hw2/MMult1.cpp
+++ hw2/MMult1.cpp
@@ -38,52 +38,167 @@ void MMult1(long m, long n, long k, double *a, double *b, double *c) {
   }
 }
 
+void MMult1blocks(long m, long n, long k, double *a, double *b, double *c) {
+  for (long j = 0; j < n/BLOCK_SIZE; j++) {
+    for (long p = 0; p < k; p++) {
+      for (long i = 0; i < m; i++) {
+          for(long j_inner = j*BLOCK_SIZE; j_inner < std::min((j+1)*BLOCK_SIZE,n); j_inner++){
+            double A_ip = a[i+p*m];
+            double B_pj = b[p+j_inner*k];
+            double C_ij = c[i+j_inner*m];
+            C_ij = C_ij + A_ip * B_pj;
+            c[i+j_inner*m] = C_ij;
+          }
+      }
+    }
+  }
+}
+
+void MMult1openmp(long m, long n, long k, double *a, double *b, double *c) {
+    #pragma omp parallel
+    #pragma omp for schedule (dynamic, 4)
+  for (long j = 0; j < n; j++) {
+    for (long p = 0; p < k; p++) {
+      for (long i = 0; i < m; i++) {
+        double A_ip = a[i+p*m];
+        double B_pj = b[p+j*k];
+        double C_ij = c[i+j*m];
+        C_ij = C_ij + A_ip * B_pj;
+        c[i+j*m] = C_ij;
+      }
+    }
+  }
+}
+
+
 int main(int argc, char** argv) {
   const long PFIRST = BLOCK_SIZE;
-  const long PLAST = 2000;
-  const long PINC = std::max(70/BLOCK_SIZE,1) * BLOCK_SIZE; // multiple of BLOCK_SIZE
+  const long PLAST = 2048+1;
+  const long PINC = 2;
 
-  printf(" Dimension       Time    Gflop/s       GB/s        Error\n");
-  for (long p = PFIRST; p < PLAST; p += PINC) {
-    long m = p, n = p, k = p;
-    long NREPEATS = 1e9/(m*n*k)+1;
-    double* a = (double*) aligned_malloc(m * k * sizeof(double)); // m x k
-    double* b = (double*) aligned_malloc(k * n * sizeof(double)); // k x n
-    double* c = (double*) aligned_malloc(m * n * sizeof(double)); // m x n
-    double* c_ref = (double*) aligned_malloc(m * n * sizeof(double)); // m x n
+    printf("\n Without Blocks\nDimension       Time    Gflop/s       GB/s        Error\n");
+    for (long p = PFIRST; p < PLAST; p *= PINC) {
+      long m = p, n = p, k = p;
+      long NREPEATS = 1e9/(m*n*k)+1;
+      double* a = (double*) aligned_malloc(m * k * sizeof(double)); // m x k
+      double* b = (double*) aligned_malloc(k * n * sizeof(double)); // k x n
+      double* c = (double*) aligned_malloc(m * n * sizeof(double)); // m x n
+      double* c_ref = (double*) aligned_malloc(m * n * sizeof(double)); // m x n
 
-    // Initialize matrices
-    for (long i = 0; i < m*k; i++) a[i] = drand48();
-    for (long i = 0; i < k*n; i++) b[i] = drand48();
-    for (long i = 0; i < m*n; i++) c_ref[i] = 0;
-    for (long i = 0; i < m*n; i++) c[i] = 0;
+      // Initialize matrices
+      for (long i = 0; i < m*k; i++) a[i] = drand48();
+      for (long i = 0; i < k*n; i++) b[i] = drand48();
+      for (long i = 0; i < m*n; i++) c_ref[i] = 0;
+      for (long i = 0; i < m*n; i++) c[i] = 0;
 
-    for (long rep = 0; rep < NREPEATS; rep++) { // Compute reference solution
-      MMult0(m, n, k, a, b, c_ref);
+      for (long rep = 0; rep < NREPEATS; rep++) { // Compute reference solution
+        MMult0(m, n, k, a, b, c_ref);
+      }
+
+      Timer t;
+      t.tic();
+      for (long rep = 0; rep < NREPEATS; rep++) {
+        MMult1(m, n, k, a, b, c);
+      }
+      double time = t.toc();
+      double flops = 2*m*n*k*NREPEATS/(1e9 * time);
+      double bandwidth = (m*k+k*n+2*m*n)*NREPEATS/(1e9 * time);
+      //double flops = 0; // TODO: calculate from m, n, k, NREPEATS, time
+      //double bandwidth = 0; // TODO: calculate from m, n, k, NREPEATS, time
+      printf("%10ld %10f %10f %10f", p, time, flops, bandwidth);
+
+      double max_err = 0;
+      for (long i = 0; i < m*n; i++) max_err = std::max(max_err, fabs(c[i] - c_ref[i]));
+      printf(" %10e\n", max_err);
+
+      aligned_free(a);
+      aligned_free(b);
+      aligned_free(c);
     }
 
-    Timer t;
-    t.tic();
-    for (long rep = 0; rep < NREPEATS; rep++) {
-      MMult1(m, n, k, a, b, c);
+    
+    printf("\nWith Blocks\n Dimension       Time    Gflop/s       GB/s        Error\n");
+    for (long p = PFIRST; p < PLAST; p *= PINC) {
+      long m = p, n = p, k = p;
+      long NREPEATS = 1e9/(m*n*k)+1;
+      double* a = (double*) aligned_malloc(m * k * sizeof(double)); // m x k
+      double* b = (double*) aligned_malloc(k * n * sizeof(double)); // k x n
+      double* c = (double*) aligned_malloc(m * n * sizeof(double)); // m x n
+      double* c_ref = (double*) aligned_malloc(m * n * sizeof(double)); // m x n
+
+      // Initialize matrices
+      for (long i = 0; i < m*k; i++) a[i] = drand48();
+      for (long i = 0; i < k*n; i++) b[i] = drand48();
+      for (long i = 0; i < m*n; i++) c_ref[i] = 0;
+      for (long i = 0; i < m*n; i++) c[i] = 0;
+
+      for (long rep = 0; rep < NREPEATS; rep++) { // Compute reference solution
+        MMult0(m, n, k, a, b, c_ref);
+      }
+
+      Timer t;
+      t.tic();
+      for (long rep = 0; rep < NREPEATS; rep++) {
+        MMult1blocks(m, n, k, a, b, c);
+      }
+      double time = t.toc();
+      double flops = 2*m*n*k*NREPEATS/(1e9 * time);
+      double bandwidth = (m*k+k*n+2*m*n)*NREPEATS/(1e9 * time);
+      //double flops = 0; // TODO: calculate from m, n, k, NREPEATS, time
+      //double bandwidth = 0; // TODO: calculate from m, n, k, NREPEATS, time
+      printf("%10ld %10f %10f %10f", p, time, flops, bandwidth);
+
+      double max_err = 0;
+      for (long i = 0; i < m*n; i++) max_err = std::max(max_err, fabs(c[i] - c_ref[i]));
+      printf(" %10e\n", max_err);
+
+      aligned_free(a);
+      aligned_free(b);
+      aligned_free(c);
     }
-    double time = t.toc();
-    double flops = 2*m*n*k*NREPEATS/(1e9 * time);
-    double bandwidth = (m*k+k*n+2*m*n)*NREPEATS/(1e9 * time);
-    //double flops = 0; // TODO: calculate from m, n, k, NREPEATS, time
-    //double bandwidth = 0; // TODO: calculate from m, n, k, NREPEATS, time
-    printf("%ld %10f %10f %10f", p, time, flops, bandwidth);
+    
+    
+    printf("\nWith OpenMP\n Dimension       Time    Gflop/s       GB/s        Error\n");
+    for (long p = PFIRST; p < PLAST; p *= PINC) {
+      long m = p, n = p, k = p;
+      long NREPEATS = 1e9/(m*n*k)+1;
+      double* a = (double*) aligned_malloc(m * k * sizeof(double)); // m x k
+      double* b = (double*) aligned_malloc(k * n * sizeof(double)); // k x n
+      double* c = (double*) aligned_malloc(m * n * sizeof(double)); // m x n
+      double* c_ref = (double*) aligned_malloc(m * n * sizeof(double)); // m x n
 
-    double max_err = 0;
-    for (long i = 0; i < m*n; i++) max_err = std::max(max_err, fabs(c[i] - c_ref[i]));
-    printf(" %10e\n", max_err);
+      // Initialize matrices
+      for (long i = 0; i < m*k; i++) a[i] = drand48();
+      for (long i = 0; i < k*n; i++) b[i] = drand48();
+      for (long i = 0; i < m*n; i++) c_ref[i] = 0;
+      for (long i = 0; i < m*n; i++) c[i] = 0;
 
-    aligned_free(a);
-    aligned_free(b);
-    aligned_free(c);
-  }
+      for (long rep = 0; rep < NREPEATS; rep++) { // Compute reference solution
+        MMult0(m, n, k, a, b, c_ref);
+      }
 
-  return 0;
+      Timer t;
+      t.tic();
+      for (long rep = 0; rep < NREPEATS; rep++) {
+        MMult1openmp(m, n, k, a, b, c);
+      }
+      double time = t.toc();
+      double flops = 2*m*n*k*NREPEATS/(1e9 * time);
+      double bandwidth = (m*k+k*n+2*m*n)*NREPEATS/(1e9 * time);
+      //double flops = 0; // TODO: calculate from m, n, k, NREPEATS, time
+      //double bandwidth = 0; // TODO: calculate from m, n, k, NREPEATS, time
+      printf("%10ld %10f %10f %10f", p, time, flops, bandwidth);
+
+      double max_err = 0;
+      for (long i = 0; i < m*n; i++) max_err = std::max(max_err, fabs(c[i] - c_ref[i]));
+      printf(" %10e\n", max_err);
+
+      aligned_free(a);
+      aligned_free(b);
+      aligned_free(c);
+    }
+    
+    return 0;
 }
 
 // * Using MMult0 as a reference, implement MMult1 and try to rearrange loops to
