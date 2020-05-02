1ac2feb175c39d1748a8ec73e60ab6ed6f0a57f5
covid19i <r.ilyeech.k@gmail.com>
2020-05-02 15:26:25 -0400

    hogwild.h third? time

--- SGD/hogwild.h
+++ SGD/hogwild.h
@@ -52,48 +52,66 @@ double getLoss(double* weight,double** trainingData,uchar* trainingLabel,int n_d
     return regularizationTerm+summ;
 }
 
-__host__ __device__ double getOneGradient(double* weight,int index,const double*trainingData,const uchar* trainingLabel,double eta,int n_data,int n_weights,int n_labels,double lambda){
-    //n_data is 2. which means i will use two data points to update one weight.
-    
-    double delta_weight=0;
-    int i=index / n_weights;//i is for label i
-    int j=index % n_weights;//j is for data j
-    //printf("%d %d %d %d %d %d \n",index,i,j,n_weights,n_data,n_labels);
-    
-    double* probList;
-    probList=(double*)malloc(n_labels*sizeof(double));
-    for(int b=0;b<n_data;b++){
-    
-        double probSum=0;
-        
-        
-        for(int l=0;l<n_labels;l++){
-            double probExp=0;
-            for(int w=0;w<n_weights;w++){
-                //printf("%d %d %d %f %f \n",l,w,b,weight[l*n_weights+w],trainingData[b*n_weights+w]);
-                probExp+=weight[l*n_weights+w]*trainingData[b*n_weights+w];
-                
-            }
-            probList[l]=exp(probExp);
-            probSum+=exp(probExp);
-            
-        }
-        
-        
-        probList[i]/=probSum;
-        //printf("probList[i]: %f \n",probList[i]);
-        
-        double sign = (trainingLabel[b]==i)?1:0;
-        double partialDerivative = (sign-probList[i])*trainingData[b*n_weights+j];
-        partialDerivative += lambda * 2 * weight[i*n_weights+j];
-        delta_weight -= partialDerivative;
-        
-    }
-    //printf("index: %d delta_weight: %f\n",index,delta_weight);
-    free(probList);
-    return delta_weight;
-    
+#define BLOCK_SIZE 1024
+
+__device__ void warpReduce(volatile double* smem, int tid) {
+  smem[tid] += smem[tid + 32];
+  smem[tid] += smem[tid + 16];
+  smem[tid] += smem[tid + 8];
+  smem[tid] += smem[tid + 4];
+  smem[tid] += smem[tid + 2];
+  smem[tid] += smem[tid + 1];
 }
+
+__global__  void run_hogwild_one_processor(double* weight, const double* trainingData, const double* trainingLabel, int data_point, double eta, int n_data, int n_weights, int n_labels, double lambda) {
+  __shared__ double smem[n_weights];//Gonna update 100% of weights in this kernel though.
+  __shared__ double denominator = 0;
+  __shared__ double numerator[n_labels];
+  __shared__ double indicator[n_labels];
+  int tid = threadIdx.x;
+  //printf("Block %d: smem[%d] = a[%d] * b[%d] == %f += %f * %f\n", blockIdx.x, tid, idx, idx, smem[tid], a[idx], b[idx]);
+  for(int i=0; i < n_labels; i++){
+    if(tid < n_weights){
+      smem[tid] = weight[i*n_weights + tid] * trainingData[data_point * n_weights + tid];
+    } else {
+      smem[tid] = 0;
+    }
+        //printf("Block %d: smem[%d] = %f\n", blockIdx.x, tid, smem[tid]);
+    __syncthreads();
+    for(unsigned int s = blockDim.x/2; s>32; s>>=1){
+        if(tid < s) {
+          smem[tid] += smem[tid+s];
+        }
+        __syncthreads();
+    }
+    if(tid < 32) warpReduce(smem, tid);
+    __syncthreads();
+    if(tid == 0){
+      numerator[i] = smem[tid];
+    }
+    __syncthreads();
+  }
+  if(tid < n_labels)
+    numerator[tid] = exp(numerator[tid]);
+  __syncthreads();
+  if(tid == 0)
+    denominator = numerator[0] + numerator[1] + numerator[2] + numerator[3] + numerator[4] +
+			numerator[5] + numerator[6] + numerator[7] + numerator[8] + numerator[9];
+  __syncthreads();
+  if(tid < n_labels){
+    numerator[tid] = numerator[tid] / denominator;
+    indicator[tid] = ((trainingLabel[data_point] == tid)?1:0);
+  __syncthreads();
+    for(int j=0; j < n_labels; j++){
+      //Lock free
+      weight[j * n_weights + tid] -= eta * ( (indicator[j] - numerator[j]) * trainingData[data_point * n_weights + tid] +
+                                             (lambda * 2 * weight[j * n_weights + tid] / n_data) );//1/n_data makes a difference?
+    }
+  }
+  __syncthreads();
+
+}
+
 
 __global__ void updateWeightKernel(double* weight,const double* trainingData,const uchar* trainingLabel,double eta,int n_data,int n_weights,int n_labels,int batchSize,double lambda,int offset){
     int x=blockIdx.x*blockDim.x+threadIdx.x;
@@ -114,55 +132,34 @@ __global__ void updateWeightKernel(double* weight,const double* trainingData,con
             int r;
             r=curand(&state)%n_data;
             label[b]=trainingLabel[r];
-            //printf("%d ",label[b]);
             for(int w=0;w<n_weights;w++){
                 data[b*n_weights+w]=trainingData[r*n_weights+w];
-                //printf("%d %d %f \n",r,w,trainingData[r*n_weights+w]);
             }
         }
-        //printf("%d %f \n",index,weight[index]);
-        
         deltaWeight=getOneGradient(weight,index, data, label,eta, batchSize, n_weights, n_labels,lambda/batchSize);
         weight[index]-=eta* deltaWeight;
-        
-        //printf("%d %f %f \n",index,deltaWeight,weight[index]);
-        
-        free(data);
-        free(label);
     }
     
 }
 
 
 
-    //define the size of cuda grid and block. after test, this is the maximum i can use
-    dim3 gridSize(4,4);
-    dim3 blockSize(5,5);
-    
-    
+    //int block_size = 785;
+    int n_blocks = 20;
+
     double* weight;
     int weight_size=(size_image+1)*10;
     cudaMallocHost((void**)&weight,weight_size*sizeof(double));
     psgd.testGPU(weight, testingData, testingLabels, n_images_test, size_image+1, 10);
     //update the weight
-    int offset=0;
     for(int j=0;j<n_iterations;j++){
-        offset=(j%20)*4*4*5*5;
-        updateWeightKernel<<<gridSize,blockSize>>>(weight,trainingData,trainingLabel,eta,n_images,size_image+1,10,2,lambda,offset);
+        updateWeightKernel<<<n_blocks, BLOCK_SIZE>>>(weight,trainingData,trainingLabel,eta,n_images,size_image+1,10,lambda);
         cudaDeviceSynchronize();
 	}
         
 
     psgd.testGPU(weight, testingData, testingLabels, n_images_test, size_image+1, 10);
- 
-    printf("end");
-    free(tempData);
-    free(tempLabel);
-    cudaFree(trainingData);
-    cudaFree(trainingLabel);
-    cudaFree(weight);
-    
-    
-    
-    
-}
+
+
+
+
