37927c8961b66881e396d1bb8badf16ebb3d3430
covid19i <r.ilyeech.k@gmail.com>
2020-05-03 15:13:18 -0400

    Changes to mnist.py for comparing with hogwildSGD.cu

--- SGD/TensorFlow/mnist.py
+++ SGD/TensorFlow/mnist.py
@@ -14,8 +14,12 @@ x_train, x_test = x_train/255., x_test/255.
 x_train = tf.reshape(x_train, shape=(-1, 784))
 x_test  = tf.reshape(x_test, shape=(-1, 784))
 
-weights = tf.Variable(tf.random.normal(shape=(784, 10), dtype=tf.float64))
-biases  = tf.Variable(tf.random.normal(shape=(10,), dtype=tf.float64))
+#weights = tf.Variable(tf.random.normal(shape=(784, 10), dtype=tf.float64))
+#biases  = tf.Variable(tf.random.normal(shape=(10,), dtype=tf.float64))
+#weights = tf.Variable(tf.zeros_initializer(shape=(784, 10), dtype=tf.float64))
+#biases  = tf.Variable(tf.zeros_initializer(shape=(10,), dtype=tf.float64))
+weights = tf.Variable(tf.zeros(shape=(784, 10), dtype=tf.float64))
+biases  = tf.Variable(tf.zeros(shape=(10,), dtype=tf.float64))
 
 def logistic_regression(x):
     lr = tf.add(tf.matmul(x, weights), biases)
@@ -54,13 +58,15 @@ start = time.time()
 for batch_numb, (batch_xs, batch_ys) in enumerate(dataset.take(n_batches), 1):
     gradients = grad(batch_xs, batch_ys)
     optimizer.apply_gradients(zip(gradients, [weights, biases]))
-    if(batch_numb < 4):
-        for j in range(490, 493):
-            print("w[%i] = %f" % (j, weights[j][0].numpy()))
+    if(batch_numb < 20):
+        for j in range(432,433):
+            print("Batch number: %i, w[%i] = %.10f" % (batch_numb, j, weights[j][0].numpy()))
     if(batch_numb % ((int)(n_batches/5)) == 0 or batch_numb == 1):
-        y_pred = logistic_regression(batch_xs)
-        loss = cross_entropy(batch_ys, y_pred)
-        acc = accuracy(batch_ys, y_pred)
+        for j in range(432,433):
+            print("Batch number: %i, w[%i] = %.10f" % (batch_numb, j, weights[j][0].numpy()))
+        y_pred = logistic_regression(x_train)
+        loss = cross_entropy(y_train, y_pred)
+        acc = accuracy(y_train, y_pred)
         print("Batch number: %i, Training loss: %f, Training accuracy: %f" % (batch_numb, loss, acc))
         y_pred_test = logistic_regression(x_test)
         loss = cross_entropy(y_test, y_pred_test)
